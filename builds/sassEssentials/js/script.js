460a59fa1e94c1fc1f0ccbdda2cfc7ecde5a5e5c
Shirak <shirag.serop@gmail.com>
2017-12-21 16:48:07 +0100

    javaScript  carusel

--- builds/sassEssentials/js/script.js
+++ builds/sassEssentials/js/script.js
@@ -14,15 +14,34 @@ btn.addEventListener('click', function(event){
       X();
     }
 });
-
 window.addEventListener("click",function(x){
   if(event.target != btn && event.target != menu && event.target != span1 && event.target != span2 && event.target != span3){
     menu.classList.remove('list_on');
     btn.classList.remove('change_btn');
   }
 });
-
-
 function X(){
   btn.classList.toggle('change_btn');
 }//EndOf menu button and menu list activity __ functions------------------------
+
+
+//carusel
+var slide = document.getElementsByClassName('carusel_item');
+var btn2 = document.getElementsByClassName('title')[0];
+
+    var num = 0;
+    var i;
+
+      btn2.onclick = function (){
+        for(i=0; i < slide.length; i++){
+          slide[i].style.display = "none";
+        }
+        num++;
+        if(num > slide.length - 1){
+          num = 0;
+        }else if(num < 0){
+          num = slide.length -1;
+        }
+        console.log(num);
+        slide[num].style.display = "block";
+      }
