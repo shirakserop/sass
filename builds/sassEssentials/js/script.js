3e742a0d32254a08658e3f72678f8f41c9d7815a
Shirak <shirag.serop@gmail.com>
2017-12-21 15:05:10 +0100

    fixing bug in menu btn and add effects in

--- builds/sassEssentials/js/script.js
+++ builds/sassEssentials/js/script.js
@@ -1,48 +1,28 @@
 
+//menu button and menu list activity __ variables------------------------
+var   btn = document.getElementsByClassName("menu_btn")[0],
+      menu = document.getElementsByClassName('list')[0],
+      span1 = document.getElementsByClassName('menu_btn_span1')[0],
+      span2 = document.getElementsByClassName('menu_btn_span2')[0],
+      span3 = document.getElementsByClassName('menu_btn_span3')[0];
+//EndOf menu button and menu list activity __ variables-------------------------
 
-
-var btn = document.getElementsByClassName("menu_btn")[0],
-    span1 = document.getElementsByClassName('menu_btn_span')[0],
-    span2 = document.getElementsByClassName('menu_btn_span')[1],
-    span3= document.getElementsByClassName('menu_btn_span')[2],
-
-  menu = document.getElementsByTagName('ul')[0];
-
-
-
-// btn.onclick = function(){
-//     if(!menu.hasAttribute("style")){
-//       menu.setAttribute("style"," transition: display .6s linear; display:block;");
-//       console.log("list_on");
-//     }else if(menu.hasAttribute("style")){
-//       menu.setAttribute("style","transition: display .6s linear;  display:none;");
-//       console.log("list_off");
-//     }
-//   }
-
-
-
-
-
-
-
-var x = false;
-  btn.addEventListener("click", function(){
-    if(x == false){
-      x = true;
-      menu.setAttribute("class","list list_on");
-    }else if (x == true){
-      x = false;
-      menu.setAttribute("class","list list_off");
+//menu button and menu list activity __ functions------------------------
+btn.addEventListener('click', function(event){
+    if(event.target = btn){
+      menu.classList.toggle('list_on');
+      X();
     }
-  });
+});
+
+window.addEventListener("click",function(x){
+  if(event.target != btn && event.target != menu && event.target != span1 && event.target != span2 && event.target != span3){
+    menu.classList.remove('list_on');
+    btn.classList.remove('change_btn');
+  }
+});
 
 
-  window.addEventListener("click",function(event){
-    if(event.target != menu && event.target != btn && event.target != span1 && event.target != span2 && event.target != span3){
-      x = false;
-      menu.setAttribute("class","list list_off");
-      //menu.setAttribute("style","transition: display .6s linear;  display:none;");
-    }
-
-  });
+function X(){
+  btn.classList.toggle('change_btn');
+}//EndOf menu button and menu list activity __ functions------------------------
